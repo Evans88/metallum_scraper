a206c9a15aa3426f67f235263d2eec3c1fb1b9c3
Daniel Evans <evansevans88@gmail.com>
2019-03-04 10:45:46 -0500

    d

--- app/Scraper.py
+++ app/Scraper.py
@@ -8,14 +8,18 @@ def scrape_band_page(url):
     soup = BeautifulSoup(html, 'html.parser')
 
     band_id = url[url.rindex("/") + 1:]
-
     band_name = soup.find("h1", {"class": "band_name"}).text
+    ret = [band_id, band_name]
+
     div_band_stats = soup.find("div", {"id": "band_stats"})
     stats_categories = [a.text.strip().replace(":", "").lower() for a in div_band_stats.find_all("dt")]
     stats_values = [re.sub("(\n|\t\s+)", "", a.text.strip()) for a in div_band_stats.find_all("dd")]
-    ret = dict(zip(stats_categories, stats_values))
-    ret['id'] = band_id
-    ret['band name'] = band_name
+    print(stats_categories)
+    print(stats_values)
+
+    ret.extend(stats_values)
+
+
 
     return ret
 
--- app/scrape_main.py
+++ app/scrape_main.py
@@ -4,26 +4,27 @@ from bs4 import BeautifulSoup
 import re
 import string
 db = db.Database()
-for i in string.ascii_uppercase:
-    try:
-        main_a = f'https://www.metal-archives.com/browse/ajax-letter/l/{i}/json/1'
-        html = request.get_raw(main_a).decode("utf-8")
-        soup = BeautifulSoup(html, 'html.parser')
+# for i in string.ascii_uppercase:
 
-        #this should work - response is not valid json, have to parse the string
-        txt = re.sub("\s+", "", soup.text)
-        max_bands_in_this_letter = int(txt[txt.find(":") + 1 :txt.find(",")])
-        print(max_bands_in_this_letter)
+main_a = f'https://www.metal-archives.com/browse/ajax-letter/l/A/json/1'
+html = request.get_raw(main_a).decode("utf-8")
+soup = BeautifulSoup(html, 'html.parser')
 
+# this should work - response is not valid json, have to parse as string
+txt = re.sub("\s+", "", soup.text)
+max_bands_in_this_letter = int(txt[txt.find(":") + 1 :txt.find(",")])
 
-    except Exception  as e:\
-            print(e)
-"""
+for display_start in range(0, max_bands_in_this_letter, 500):
+
+    main_a = f'https://www.metal-archives.com/browse/ajax-letter/l/A/json/1?DisplayStart={display_start}'
+    html = request.get_raw(main_a).decode("utf-8")
+    soup = BeautifulSoup(html, 'html.parser')
     for href in soup.findAll("a", href=True):
 
         # follow href to band page
         band_details = sb.scrape_band_page(href['href'])
         db.insert_into_band(band_details, auto_commit=True)
-"""
+
+
 
 
